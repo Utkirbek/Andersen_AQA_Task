2579f249ec0d71b0ad6a921f95de88a43fcc314f
Utkirbek <dovutkhanmutaliyev@gmail.com>
2023-11-19 16:37:06 +0300

    second task

---
+++ task2.js
@@ -0,0 +1,3 @@
+// first of this "[((())()(())]]" sequence of brackets are not considered correct 
+// to correctthis sequance we can add one opening square bracket or remove the closing square bracket
+ 
